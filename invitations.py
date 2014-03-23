4c3beb407e15e868847f516db7d4c6f727182e26
Siôn Le Roux <sinisterstuf@gmail.com>
2014-03-23 12:14:17 +0100

    Move templates to own directory
    The tex templates were starting to clutter up a bit so I moved the
    elsewhere, this'll also help if you've got more templates than I do.

--- invitations.py
+++ invitations.py
@@ -3,21 +3,22 @@ import csv
 from string import Template
 
 guests = csv.DictReader(open('invite-list.csv'))
+tdir = './templates/'
 
 # when the counter is even we're at the end of the page, so: \clearpage
 # when it's odd we're in the middle, so \hspace
 seperators = ('\clearpage', '\hspace{2cm}')
 counter = 1
 
-print(open('preamble.tex').read())
+print(open(tdir + 'preamble.tex').read())
 print('\\begin{document}')
 
 for guest in guests:
-    template = Template(open(guest['language'] + '.tex').read().rstrip('\n'))
+    template = Template(open(tdir + guest['language'] + '.tex').read().rstrip('\n'))
 
     # set lagzi for re-use in template, it's read from another template
     if guest['lagzi'] == 'y':
-        guest['lagzi'] = open(guest['language'] + '_lagzi.tex').read()
+        guest['lagzi'] = open(tdir + guest['language'] + '_lagzi.tex').read()
     elif guest['lagzi'] == 'n':
         guest['lagzi'] = ''
     else:
